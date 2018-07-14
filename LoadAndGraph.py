92110a7afc7ba7e85b2d75767dcd16ea82719a0c
Mathieu Boretti <mathieu.boretti@gmail.com>
2018-07-14 16:49:02 +0200

    Sensor(s) refactoring (#1)

--- LoadAndGraph.py
+++ LoadAndGraph.py
@@ -46,6 +46,16 @@ def main(argv=None):
         dest="output", help="path to the output folder", type=validateOutput)
     parser.add_argument('--5min', '-5', dest="by5min",
                         action='store_true', help="aggregate value by 5min")
+    parser.add_argument('--verbose', '-v', dest="verbose",
+                        action='store_true', help="generate everything")
+    parser.add_argument('--details', '-d', dest="detail",
+                        action='store_true', help="generate detailled charts (-v also generate -d)")
+    parser.add_argument('--meta', '-m', dest="meta",
+                        action='store_true', help="generate chart by metasensor (-v also generate -m)")
+    parser.add_argument('--global', '-g', dest="globalc",
+                        action='store_true', help="generate global chart (-v also generate -g)")
+    parser.add_argument('--ext-vs-int', dest="extvsint",
+                        action='store_true', help="generate external vs internal (-v also generate -ext-vs-int)")
     arg = parser.parse_args()
 
     if not os.path.isdir(arg.output):
@@ -65,10 +75,31 @@ def main(argv=None):
             of.close()
 
     else:
+        mergeFunction = sensor.dateTimeTo5Minute(
+        ) if arg.by5min else sensor.dateTimeToMinute()
+
+        def groupFunction(
+            n): return 'Extérieur' if n == 'Extérieur' else 'Intérieur'
+
+        def filteroutFunction(n): return n.unit == 'V'
+
+        def metaFunction(sensorsmap):
+            meta = {}
+            for n, s in sensorsmap.items():
+                target = n
+                if ' - T' in n:
+                    target = n.replace(' - T', '')
+                elif ' - V' in n:
+                    target = n.replace(' - V', '')
+                if ' - RH' in n:
+                    target = n.replace(' - RH', '')
+                if target not in meta:
+                    meta[target] = {}
+                meta[target][n] = s
+            return meta
         print('Reading from {}'.format(arg.input))
-        data = sensors(arg.input, sensor.dateTimeTo5Minute() if arg.by5min else sensor.dateTimeToMinute(
-        ), lambda n: 'Extérieur' if n == 'Extérieur' else 'Intérieur')
-        data.filterOutSensor(sensor.sensorIsUnit('V'))
+        data = sensors(arg.input, mergeFunction, groupFunction,
+                       metaFunction, filteroutFunction)
         print('Storing to cache {}'.format(cachename))
         of = open(cachename, "wb")
         pickle.dump(data, of)
@@ -76,18 +107,89 @@ def main(argv=None):
 
     print('Datas are :\n{}'.format(data))
 
-    for n, fd in sorted(data.toMultiFigures().items()):
-        print('Writing to subfolder {}'.format(n))
-        if n == '/':
-            n = ''
-        targetfolder = os.path.join(arg.output, n)
-        if not os.path.exists(targetfolder):
-            os.mkdir(targetfolder)
-        for sn, d in sorted(fd.items()):
-            output = os.path.join(targetfolder, sn.translate(
-                str.maketrans('/:\\', '---')) + ".html")
-            print('Writing to {}'.format(output))
-            plot(d, filename=output, auto_open=False)
+    def logAndPlot(output, figure):
+        print('Writing to {}'.format(output))
+        plot(figure, filename=output, auto_open=False)
+
+    if arg.verbose or arg.globalc:
+        def sensorIsUnitAndClazz(unit, clazz):
+            return lambda v: v.clazz == clazz and v.unit == unit
+
+        # All temp
+        logAndPlot(os.path.join(arg.output, 'Toutes les températures.html'), data.toFigure(
+            sensorIsUnitAndClazz('°C', 'Sensor'), '°C', 'Toutes les températures'))
+
+        # All rh
+        logAndPlot(os.path.join(arg.output, 'Toutes les humidités.html'), data.toFigure(
+            sensorIsUnitAndClazz('RH%', 'Sensor'), '°C', 'Toutes les humidités'))
+
+        # All temp
+        logAndPlot(os.path.join(
+            arg.output, 'Toutes les températures - Baseline.html'), data.toFigure(
+            sensorIsUnitAndClazz('°C', 'Sensor->Baseline'), '°C', 'Toutes les températures - Baseline'))
+
+        # All rh
+        logAndPlot(os.path.join(
+            arg.output, 'Toutes les humidité - Baselines.html'), data.toFigure(
+            sensorIsUnitAndClazz('RH%', 'Sensor->Baseline'), '°C', 'Toutes les humidités - Baseline'))
+
+    if arg.verbose or arg.extvsint:
+        # Interieur vs exterieur
+        external = data['Extérieur'].asScatter()
+        internalt = data['Intérieur - Sensor [°C] / Mean'].asScatter(
+            name='Température intérieur', minSensor=data['Intérieur - Sensor [°C] / Min'], maxSensor=data['Intérieur - Sensor [°C] / Max'])
+        internalrh = data['Intérieur - Sensor [RH%] / Mean'].asScatter(
+            name='Humidité intérieur', minSensor=data['Intérieur - Sensor [RH%] / Min'], maxSensor=data['Intérieur - Sensor [RH%] / Max'], yaxis='y2')
+        external_b = data['Extérieur - baseline'].asScatter()
+        internalt_b = data['Intérieur - Sensor [°C] / Mean - baseline'].asScatter(
+            name='Température intérieur / Baseline')
+        internalrh_b = data['Intérieur - Sensor [RH%] / Mean - baseline'].asScatter(
+            name='Humidité intérieur / Baseline')
+        logAndPlot(os.path.join(
+            arg.output, 'Comparaison intérieur vs extérieur.html'), sensors.scattersToFigure(
+            [external, internalt, internalrh], '°C', 'Intérieur vs Extérieur', 'RH%'))
+        logAndPlot(os.path.join(
+            arg.output, 'Comparaison intérieur vs extérieur - Baselines.html'), sensors.scattersToFigure(
+            [external, internalt, internalrh, external_b, internalt_b, internalrh_b], '°C', 'Intérieur vs Extérieur - avec baselines', 'RH%'))
+        logAndPlot(os.path.join(
+            arg.output, 'Comparaison intérieur vs extérieur - Uniquement baselines.html'), sensors.scattersToFigure(
+            [external_b, internalt_b, internalrh_b], '°C', 'Intérieur vs Extérieur - uniquement baselines', 'RH%'))
+
+    if arg.verbose or arg.meta:
+        # Generate one file per meta sensor with/without baseline
+
+        def plotIfExist(output, figure):
+            if len(figure['data']) > 0:
+                logAndPlot(output, figure)
+
+        for name, lst in data.metassensors:
+            folder = os.path.join(arg.output, name.translate(
+                str.maketrans('/:\\', '---')))
+            if not os.path.isdir(folder):
+                os.mkdir(folder)
+
+            fig = data.toFigure(
+                lambda s: s.name in lst and s.clazz == 'Sensor', '°C', name, '%RH', lambda s: 'y' if s.unit == '°C' else 'y2')
+            plotIfExist(os.path.join(folder, 'mesures.html'), fig)
+
+            fig = data.toFigure(
+                lambda s: s.name in lst and s.clazz == 'Sensor->Baseline', '°C', name + "- Baselines", '%RH', lambda s: 'y' if s.unit == '°C' else 'y2')
+            plotIfExist(os.path.join(folder, 'baselines.html'), fig)
+
+            fig = data.toFigure(
+                lambda s: s.name in lst and s.clazz in ('Sensor', 'Sensor->Baseline'), '°C', name + "- Mesures et Baselines", '%RH', lambda s: 'y' if s.unit == '°C' else 'y2')
+            plotIfExist(os.path.join(folder, 'mesures et baselines.html'), fig)
+
+            fig = data.toFigure(
+                lambda s: s.name in lst, '°C', name + "- Tous", '%RH', lambda s: 'y' if s.unit == '°C' else 'y2')
+            plotIfExist(os.path.join(folder, 'tous.html'), fig)
+
+    if arg.verbose or arg.detail:
+        # Generate one file per sensor
+        os.mkdir(os.path.join(arg.output, 'details'))
+        for name, s in data.items():
+            logAndPlot(os.path.join(arg.output, 'details', name.translate(
+                str.maketrans('/:\\', '---')) + ".html"), sensors.sensorToFigure(s, s.name))
 
 
 if __name__ == "__main__":
---
+++ tests/test_sensors.py
@@ -0,0 +1,100 @@
+'''
+Created on 10 juil. 2018
+
+@author: borettim
+'''
+
+from pytest import fixture
+
+from areexsupport.sensors import sensors
+from datetime import datetime
+from pytz import timezone
+import textwrap
+
+
+@fixture(scope="session")
+def simpleinput(tmpdir_factory):
+    fn = tmpdir_factory.mktemp("data").join("input.txt")
+    ffn = open(fn, 'w')
+    try:
+        ffn.write(textwrap.dedent('''
+        Measurement log
+
+        Sensors:
+        
+        Column    Sensor    Unit
+        1    Maquette Partie Haute - V    V
+        2    Maquette Partie Haute - RH    RH%
+        3    Maquette Partie Haute - T    °C
+        4    Extérieur    °C
+        
+        Start date: 14.07.2018
+        End date: 14.07.2018
+        
+        Time    1    2    3    4
+        14.07.2018 00:00:39     3.2        23.4    
+        14.07.2018 00:00:40                25.2
+        14.07.2018 00:01:07        48.2        
+        14.07.2018 00:01:29            23.4    
+        14.07.2018 00:01:31                25.2
+        14.07.2018 00:01:47        48.2        
+        14.07.2018 00:02:09                25.2
+        14.07.2018 00:02:33            23.4    
+        14.07.2018 00:03:00                25.2
+        14.07.2018 00:03:04        48.2        
+        14.07.2018 00:03:44            23.4    
+        14.07.2018 00:03:46                25.2
+        14.07.2018 00:04:40        48.2        
+        14.07.2018 00:04:52                25.1
+        14.07.2018 00:05:27            23.4    
+        14.07.2018 00:05:58        48.2        
+        14.07.2018 00:06:04                25.2
+        14.07.2018 00:06:21            23.4    
+        14.07.2018 00:06:40        48.2        
+        14.07.2018 00:07:13                25.1
+        14.07.2018 00:07:25            23.3    
+        14.07.2018 00:07:55        48.2        
+        14.07.2018 00:08:17            23.3    
+        14.07.2018 00:08:22                25.1
+        14.07.2018 00:08:35        48.2        
+        14.07.2018 00:09:22            23.3    
+        14.07.2018 00:09:33                25.1
+        14.07.2018 00:10:15        48.2        
+        14.07.2018 00:10:35                25.1
+        14.07.2018 00:11:01            23.3    
+        14.07.2018 00:11:12                25.1
+        14.07.2018 00:11:51        48.2        
+        14.07.2018 00:12:23            23.3    
+        14.07.2018 00:12:27                25.1
+        14.07.2018 00:12:46        48.2        
+        14.07.2018 00:13:29                25.1
+        14.07.2018 00:13:30            23.3    
+        14.07.2018 00:13:59        48.2        
+        14.07.2018 00:14:01                25.1
+        14.07.2018 00:14:40            23.3    
+        14.07.2018 00:15:13                25.1
+        14.07.2018 00:15:36        48.2        
+        14.07.2018 00:16:01                25.0
+        14.07.2018 00:16:24            23.4    
+        14.07.2018 00:17:03                25.0
+        14.07.2018 00:17:12        48.2        
+        14.07.2018 00:17:42                25.0
+        14.07.2018 00:17:44            23.4    
+        14.07.2018 00:18:07        48.2        
+        14.07.2018 00:18:26            23.4    
+        14.07.2018 00:18:33                25.0
+        14.07.2018 00:18:43        48.2        
+        14.07.2018 00:19:27     3.2            ''').replace('    ', '\t'))
+    finally:
+        ffn.close()
+    return fn
+
+
+def testSensorsInitOnlyMandataParameter(simpleinput):
+    d = sensors(simpleinput)
+    assert 'Extérieur' in d.keys()
+    assert 'Maquette Partie Haute - RH' in d.keys()
+    assert 'Maquette Partie Haute - T' in d.keys()
+    assert 'Maquette Partie Haute - V' in d.keys()
+    assert len(d.groups) == 1
+    assert 'default' in d.groups
